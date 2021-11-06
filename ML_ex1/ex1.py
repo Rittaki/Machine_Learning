9063499a70c584391f56e5fe3d9d932148c25329
rittaki <com.margo@gmail.com>
2021-11-06 16:37:18 +0200

    works for all sizes of k

--- ML_ex1/ex1.py
+++ ML_ex1/ex1.py
@@ -8,8 +8,8 @@ z = np.loadtxt(centroids_fname) #load centroids
 number_of_clusters = z.shape[0]
 collection_of_clusters = dict()
 
-for i in range(number_of_clusters):
-    collection_of_clusters["cluster" + str(i)] = []
+# for i in range(number_of_clusters):
+#     collection_of_clusters["cluster" + str(i)] = []
 
 orig_pixels = plt.imread(image_fname)
 pixels = orig_pixels.astype(float)/255.
@@ -18,7 +18,7 @@ pixels = pixels.reshape(-1, 3)
 # for i, pixel in enumerate(pixels):
 #
 
-new_centroids = []
+# new_centroids = []
 old_centroids = z
 iterations = 0
 while True:
@@ -27,6 +27,7 @@ while True:
     distances = []
     for i in range(number_of_clusters):
         collection_of_clusters["cluster" + str(i)] = []
+
     for i, center in enumerate(old_centroids):
         dist_for_single_centroid = np.linalg.norm(center - pixels, axis=1)
         distances.append(dist_for_single_centroid)
@@ -34,23 +35,42 @@ while True:
         print("min dist ", np.min(dist_for_single_centroid))
 
     count = 0
-    for i in distances[0]:
-        if distances[0][count] < distances[1][count]:
-            collection_of_clusters["cluster" + str(0)].append(pixels[count])
-        else:
-            collection_of_clusters["cluster" + str(1)].append(pixels[count])
-        # print(distances[0][count])
-        count = count + 1
+    len_of_dists = len(distances[0])
+    # for i in range(len_of_dists):
+    #
+    array_of_mins = np.min(distances, axis=0)
+    for i in range(len_of_dists):
+        for j in range(number_of_clusters):
+            if distances[j][i] == array_of_mins[i]:
+                collection_of_clusters["cluster" + str(j)].append(pixels[i])
+    # for i in distances[0]:
+    #     if distances[0][count] < distances[1][count]:
+    #         collection_of_clusters["cluster" + str(0)].append(pixels[count])
+    #     else:
+    #         collection_of_clusters["cluster" + str(1)].append(pixels[count])
+    #     # print(distances[0][count])
+    #     count = count + 1
+    new_centroids = []
+    for i in range(number_of_clusters):
+        np_collection_of_clusters = np.array(collection_of_clusters["cluster" + str(i)])
+        avg = np.array(np.average(np_collection_of_clusters.reshape(-1, 3), axis=0).round(4))
+        # new_centroids = []
+        new_centroids.append(avg)
 
-    np_collection_of_clusters_first = np.array(collection_of_clusters["cluster" + str(0)])
+    new_centroids = np.array(new_centroids)
 
-    avg_first = np.array(np.average(np_collection_of_clusters_first.reshape(-1, 3), axis=0).round(4))
-    np_collection_of_clusters_second = np.array(collection_of_clusters["cluster" + str(1)])
-    avg_second = np.array(np.average(np_collection_of_clusters_second.reshape(-1, 3), axis=0).round(4))
-    new_centroids = np.vstack([avg_first, avg_second])
-
-    print(avg_first)
-    print(avg_second)
+    # np_collection_of_clusters_first = np.array(collection_of_clusters["cluster" + str(0)])
+    # np_collection_of_clusters_second = np.array(collection_of_clusters["cluster" + str(1)])
+    # avg_first = np.array(np.average(np_collection_of_clusters_first.reshape(-1, 3), axis=0).round(4))
+    # avg_second = np.array(np.average(np_collection_of_clusters_second.reshape(-1, 3), axis=0).round(4))
+    # new_centroids = np.vstack([avg_first, avg_second])
+    #
+    # stam_centroids = []
+    # stam_centroids.append(avg_first)
+    # stam_centroids.append(avg_second)
+    # stam_centroids = np.array(stam_centroids)
+    # print(avg_first)
+    # print(avg_second)
 
     f.write(f"[iter {iterations}]:{','.join([str(i) for i in new_centroids])}")
     f.write("\n")
