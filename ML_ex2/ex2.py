14d1d775dbe6c131339337068cd51aa67381f154
rittaki <com.margo@gmail.com>
2021-11-16 23:09:01 +0200

    added passive-aggressive

--- ML_ex2/ex2.py
+++ ML_ex2/ex2.py
@@ -2,11 +2,36 @@ import numpy as np
 import sys
 import random
 
-def perceptron(train_x, train_y):
-    eta = 0.1 # change later
-    epochs = 20 # change later
+def svm(train_x, train_y):
+    return
+
+def passive_aggressive(train_x, train_y):
+    epochs = 50 # change later
     weights = np.zeros((3, train_x.shape[1]))
     for t in range(epochs):
+        # shuffled = list(zip(train_x, train_y))
+        # np.random.shuffle(shuffled)
+        # x, y = zip(*shuffled)
+        for x_i, y_i in zip(train_x, train_y):
+            y_hat = np.argmax(np.dot(weights, x_i))
+            if y_hat != int(y_i):
+                loss = max(0, 1 - np.dot(weights[int(y_i)], x_i) + np.dot(weights[y_hat], x_i))
+                norm = 2 * ((np.linalg.norm(x_i)) ** 2)
+                if norm != 0:
+                    tau = loss/norm
+                    weights[int(y_i), :] = weights[int(y_i), :] + tau * x_i
+                    weights[y_hat, :] = weights[y_hat, :] - tau * x_i
+    return weights
+
+
+def perceptron(train_x, train_y):
+    eta = 0.1 # change later
+    epochs = 50 # change later
+    weights = np.zeros((3, train_x.shape[1]))
+    for t in range(epochs):
+        # shuffled = list(zip(train_x, train_y))
+        # np.random.shuffle(shuffled)
+        # x, y = zip(*shuffled)
         for x_i, y_i in zip(train_x, train_y):
             y_hat = np.argmax(np.dot(weights, x_i))
             if y_hat != int(y_i):
@@ -31,6 +56,16 @@ def z_score(examples):
         if deviation != 0:
             examples[:, i] = (examples[:, i] - mean) / deviation
 
+def test(test_examples, pa_weights, per_weights, out):
+    # number of tests
+    m = test_examples.shape[0]
+    z_score(test_examples)
+    for i in range(m):
+        perceptron_yhat = np.argmax(np.dot(per_weights, test_examples[i]))
+        pa_yhat = np.argmax(np.dot(pa_weights, test_examples[i]))
+        # out.write(f"knn: {knn_yhat}, perceptron: {perceptron_yhat}, svm: {svm_yhat}, pa: {pa_yhat}\n")
+        out.write(f"perceptron: {perceptron_yhat}, pa: {pa_yhat}\n")
+
 def main():
     train_x_path, train_y_path, test_x_path, out_file = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     out = open(out_file, "a")
@@ -40,6 +75,9 @@ def main():
     # minmax(train_x)
     z_score(train_x)
     perceptron_weights = perceptron(train_x, train_y)
+    pa_weights = passive_aggressive(train_x, train_y)
+    # svm_weights = svm(train_x, train_y)
+    test(test_x, pa_weights, perceptron_weights, out)
     print("Rita")
 
 if __name__ == '__main__':
