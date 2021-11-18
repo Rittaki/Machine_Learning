961937b0583ef45e56db9b0be5bf47c5a77ac1cf
rittaki <com.margo@gmail.com>
2021-11-18 14:36:18 +0200

    algorithms work, needs improvement

--- ML_ex2/ex2.py
+++ ML_ex2/ex2.py
@@ -2,15 +2,20 @@ import numpy as np
 import sys
 import random
 
-# def knn(train_x, train_y, test_x):
-#     epochs = 50 # change later
-#     k = 5 # change later
-#     weights = np.zeros((3, train_x.shape[1]))
-#     for t in range(epochs):
-#         for x_test in test_x:
-#
-#
-#     return weights
+def knn(train_x, train_y, x_test):
+    k = 10 # change later
+    classes = [0, 0, 0]
+    distances = []
+    distances = [np.linalg.norm(x_test - x_i) for x_i in train_x]
+    sorted_dist = sorted(distances)
+    k_distances = sorted_dist[: k]
+    k_neighbours = []
+    for dist in k_distances:
+        k_neighbours.append(train_x[distances.index(dist)])
+    for neighbour in k_neighbours:
+        index = np.where(np.all(neighbour == train_x, axis=1))[0][0]
+        classes[int(train_y[index])] += 1
+    return np.argmax(classes)
 
 def passive_aggressive(train_x, train_y):
     epochs = 50 # change later
@@ -78,16 +83,17 @@ def z_score(examples):
         if deviation != 0:
             examples[:, i] = (examples[:, i] - mean) / deviation
 
-def test(test_examples, pa_weights, per_weights, svm_weights, out):
+def test(test_examples, pa_weights, per_weights, svm_weights, out, train_x, train_y, test_for_knn):
     # number of tests
     m = test_examples.shape[0]
     z_score(test_examples)
     for i in range(m):
+        knn_yhat = knn(train_x, train_y, test_for_knn[i])
         perceptron_yhat = np.argmax(np.dot(per_weights, test_examples[i]))
         pa_yhat = np.argmax(np.dot(pa_weights, test_examples[i]))
         svm_yhat = np.argmax(np.dot(svm_weights, test_examples[i]))
-        # out.write(f"knn: {knn_yhat}, perceptron: {perceptron_yhat}, svm: {svm_yhat}, pa: {pa_yhat}\n")
-        out.write(f"perceptron: {perceptron_yhat}, svm: {svm_yhat}, pa: {pa_yhat}\n")
+        out.write(f"knn: {knn_yhat}, perceptron: {perceptron_yhat}, svm: {svm_yhat}, pa: {pa_yhat}\n")
+        # out.write(f"perceptron: {perceptron_yhat}, svm: {svm_yhat}, pa: {pa_yhat}\n")
 
 def main():
     train_x_path, train_y_path, test_x_path, out_file = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
@@ -95,6 +101,9 @@ def main():
     test_x = np.loadtxt(test_x_path, delimiter=',')
     train_x = np.loadtxt(train_x_path, delimiter=',')
     train_y = np.genfromtxt(train_y_path)
+    train_x_for_knn = train_x.copy()
+    train_y_for_knn = train_y.copy()
+    test_x_for_knn = test_x.copy()
     # minmax(train_x)
     z_score(train_x)
     z_score(test_x)
@@ -102,7 +111,7 @@ def main():
     pa_weights = passive_aggressive(train_x, train_y)
     svm_weights = svm(train_x, train_y)
     # knn_weights = knn(train_x, train_y, test_x)
-    test(test_x, pa_weights, perceptron_weights, svm_weights, out)
+    test(test_x, pa_weights, perceptron_weights, svm_weights, out, train_x_for_knn, train_y_for_knn, test_x_for_knn)
     print("Rita")
 
 if __name__ == '__main__':
