1fc130ecdb79a0b882bfc18734352434d0009a74
rittaki <com.margo@gmail.com>
2021-12-21 00:06:36 +0200

    updated for test

--- ML_ex4/ex4.py
+++ ML_ex4/ex4.py
@@ -1,26 +1,30 @@
 import sys
+import torch
 import numpy as np
-import torch, torchvision
+import torchvision
+from torch.utils import data
+import matplotlib.pyplot as plt
 import torch.nn as nn
+import torch
 import torch.nn.functional as F
-from torch.utils.data import Dataset
 
-class MyData(Dataset):
-    def __init__(self, x, y=None, transforms=None):
-        self.data_x = x
-        self.data_y = y
-        self.transforms = transforms
+class MyDataSet(data.Dataset):
+    def __init__(self, x, y=None, transform=None):
+        self.__x = x
+        self.__y = y
+        self.__transform = transform
 
     def __len__(self):
-        return len(self.data_x)
+        return len(self.__x)
 
-    def __getitem__(self, item):
-        x = self.data_x[item]
+    def __getitem__(self, index):
+        x = self.__x[index]
         x = np.asarray(x).astype(np.uint8).reshape(28, 28)
-        if self.transforms:
-            x = self.transforms(x)
-        if self.data_y is not None:
-            return x, self.data_y[item]
+        if self.__transform:
+            x = self.__transform(x)
+        if self.__y is not None:
+            y = self.__y[index]
+            return x, y
         return x
 
 class ModelA(nn.Module):
@@ -49,21 +53,21 @@ def split(train_x, train_y):
 
 def main():
     train_x = np.loadtxt(sys.argv[1])
-    train_y = np.loadtxt(sys.argv[2])
+    train_y = np.loadtxt(sys.argv[2], dtype="int64")
     test_x = np.loadtxt(sys.argv[3])
     x_data, y_data, x_val, y_val = split(train_x, train_y)
     transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
                                     torchvision.transforms.Normalize((0.1307,), (0.3081,))])
-    train_data = MyData(x_data, y_data, transforms)
-    validation_data = MyData(x_val, y_val, transforms)
-    test_data = MyData(test_x, None, transforms)
+    train_data = MyDataSet(x_data, y_data, transforms)
+    validation_data = MyDataSet(x_val, y_val, transforms)
+    test_data = MyDataSet(test_x, None, transforms)
     # print(len(train_data))
     # print(train_data[1000])
     # print(len(test_data))
     # print(test_data[1000])
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
     valid_loader = torch.utils.data.DataLoader(validation_data, batch_size=64, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_data)
     model_a = ModelA(image_size=28*28)
 
     lr = 0.1
@@ -72,14 +76,59 @@ def main():
     for epoch in range(1, 10 + 1):
         train(epoch, model_a, train_loader, optimizer)
 
+    prediction = test(model_a, test_loader)
+    write_to_file(prediction)
+
 def train(epoch, model, train_loader, optimizer):
     model.train()
+    losses = 0
+    accuracy_counter = 0
     for batch_idx, (data, labels) in enumerate(train_loader):
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, labels)
+        losses += F.nll_loss(output, labels, reduction="mean").item()
         loss.backward()
         optimizer.step()
+        prediction = output.max(1, keepdim=True)[1]
+        accuracy_counter += prediction.eq(labels.view_as(prediction)).cpu().sum()
+
+
+    # Get the average loss
+    losses /= (len(train_loader) * 64)
+    print('\nTrain Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        losses, accuracy_counter, (len(train_loader) * 64),
+        100. * accuracy_counter / (len(train_loader) * 64)))
+
+# def (model, test_loader):
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     with torch.no_grad():
+#         for data, target in test_loader:
+#             output = model(data)
+#             test_loss += F.nll_loss(output, target, size_average=False).item()
+#             pred = output.max(1, keepdim=True)[1]
+#             correct += pred.eq(target.view_as(pred)).cpu().sum()
+#     test_loss /= len(test_loader.dataset)
+#     print('\nTestset: Average loss: {:.4f}, Accuracy: {} / {}({:.0f} % )\n'.format(
+#         test_loss, correct, len(test_loader.dataset),
+#         100. * correct / len(test_loader.dataset)))
+
+def test(model, test_loader):
+    model.eval()
+    test_y_list = []
+    for data in test_loader:
+        output = model(data)
+        predict = output.max(1, keepdim=True)[1]
+        test_y_list.append(str(int(predict)))
+    return test_y_list
+
+def write_to_file(predict_y):
+    with open('test_y', 'w') as file:
+        for y in predict_y:
+            file.write("%s\n" % y)
+    file.close()
 
 if __name__ == '__main__':
     main()
