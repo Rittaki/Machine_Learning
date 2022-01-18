d0c1de4160424e0b43c1f9e732e2c04814d9f9c1
rittaki <com.margo@gmail.com>
2022-01-18 23:03:45 +0200

    added dropout - accuracy ~90%

--- ML_ex5/ex5.py
+++ ML_ex5/ex5.py
@@ -31,18 +31,22 @@ class CNNnetwork(nn.Module):
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.MaxPool2d(kernel_size=2))
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.MaxPool2d(kernel_size=2))
         self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.MaxPool2d(kernel_size=2))
         self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.MaxPool2d(kernel_size=2))
         # self.flatten = nn.Flatten()
         self.linear = nn.Linear(256 * 10 * 6, 30)
