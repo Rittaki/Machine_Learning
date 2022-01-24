a920cab56197c0bbf1d4843261e88ad265d2e98e
rittaki <com.margo@gmail.com>
2022-01-24 14:15:03 +0200

    final model with dropout 0.4 - accuracy >90-91%

--- ML_ex5/ex5.py
+++ ML_ex5/ex5.py
@@ -1,7 +1,5 @@
 import os
-import sys
 import torch
-import numpy as np
 import torchvision
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
@@ -13,11 +11,15 @@ from gcommand_dataset import GCommandLoader
 
 cuda = torch.cuda.is_available()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+# print(device)
 
 trainData = GCommandLoader('./data/train')
 validData = GCommandLoader('./data/valid')
 testData = GCommandLoader('./data/test')
+# trainData = GCommandLoader('./gcommands/train')
+# validData = GCommandLoader('./gcommands/valid')
+# testData = GCommandLoader('./gcommands/test')
+
 
 classes = {0: "bed", 1: "bird", 2: "cat", 3: "dog", 4: "down", 5: "eight", 6: "five", 7: "four", 8: "go", 9: "happy",
            10: "house", 11: "left", 12: "marvin", 13: "nine", 14: "no", 15: "off", 16: "on", 17: "one", 18: "right",
@@ -31,24 +33,23 @@ class CNNnetwork(nn.Module):
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.4),
             nn.MaxPool2d(kernel_size=2))
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.4),
             nn.MaxPool2d(kernel_size=2))
         self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.4),
             nn.MaxPool2d(kernel_size=2))
         self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.4),
             nn.MaxPool2d(kernel_size=2))
-        # self.flatten = nn.Flatten()
         self.linear = nn.Linear(256 * 10 * 6, 30)
 
     def forward(self, x):
@@ -72,7 +73,6 @@ def load_data():
 
 
 def train(epoch, model, trainLoader, optimizer, criterion):
-    if_index = 0
     model.train()
     losses = 0
     accuracy_counter = 0
@@ -84,25 +84,15 @@ def train(epoch, model, trainLoader, optimizer, criterion):
         loss = criterion(output, labels)
         loss.backward()
         optimizer.step()
-        # for idx, (data, labels) in enumerate(trainLoader):
-        #     optimizer.zero_grad()
-        #     output = model(data)
-        #     loss = criterion(output, labels)
-        #     loss.backward()
-        #     optimizer.step()
-
         losses += loss.item()
         prediction = output.max(1, keepdim=True)[1]
         accuracy_counter += prediction.eq(labels.view_as(prediction)).cpu().sum()
-        if idx % 200 == 199:
-            if_index += 1
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, idx + 1, losses / (if_index * 200)))
 
     losses /= len(trainLoader)
-    print('\nEpoch: {}, Train Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        epoch, losses, accuracy_counter, size, 100. * accuracy_counter / size))
-    # accuracy = 100. * accuracy_counter / len(trainLoader)
+    # print('\nEpoch: {}, Train Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    #     epoch, losses, accuracy_counter, size, 100. * accuracy_counter / size))
+    accuracy = 100. * accuracy_counter / size
+    return losses, accuracy
 
 
 def valid(model, valid_loader, criterion):
@@ -116,12 +106,13 @@ def valid(model, valid_loader, criterion):
             output = model(images)
             valid_loss = criterion(output, labels)
             losses += valid_loss.item()
-
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(labels.view_as(pred)).cpu().sum()
     losses /= len(valid_loader)
-    print('\nValid Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        losses, correct, size, 100. * correct / size))
+    # print('\nValid Set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    #     losses, correct, size, 100. * correct / size))
+    accuracy = 100. * correct / size
+    return losses, accuracy
 
 
 def test(model, test_loader):
@@ -131,17 +122,14 @@ def test(model, test_loader):
     i = 0
     for data in test_loader:
         output = model(data[0])
-        _, batch_pred = torch.max(output.data, 1)
-        batch_pred = batch_pred.tolist()
-        for predict in batch_pred:
+        _, pred = torch.max(output.data, 1)
+        pred = pred.tolist()
+        for predict in pred:
             name = classes[predict]
             file_name = os.path.basename(files_name[i][0])
             test_y_list.append("{},{}".format(file_name, name))
             i += 1
     test_y_list = sorted(test_y_list, key=lambda x: int(x.split('.')[0]))
-    # predict = output.max(1, keepdim=True)[1]
-    # predict = predict.tolist()
-    # test_y_list.append(str(int(predict)))
     return test_y_list
 
 
@@ -152,6 +140,28 @@ def write_to_file(predict_y):
     file.close()
 
 
+def draw_loss(train_loss_values, valid_loss_values):
+    epochs = range(1, len(train_loss_values) + 1)
+    plt.plot(epochs, train_loss_values, label='Train')
+    plt.plot(epochs, valid_loss_values, label='Validation')
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title("Loss Graph")
+    plt.legend()
+    plt.show()
+
+
+def draw_accuracy(train_accuracy, valid_accuracy):
+    epochs = range(1, len(train_accuracy) + 1)
+    plt.plot(epochs, train_accuracy, label='Train')
+    plt.plot(epochs, valid_accuracy, label='Validation')
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy Graph")
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
     trainLoader, validLoader, testLoader = load_data()
     lr = 0.001
@@ -159,10 +169,21 @@ if __name__ == "__main__":
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    for epoch in range(20):
-        print(f"epoch number {epoch}")
-        train(epoch, model, trainLoader, optimizer, criterion)
-        valid(model, validLoader, criterion)
+
+    train_loss_list = list()
+    train_accuracy_list = list()
+    valid_loss_list = list()
+    valid_accuracy_list = list()
+
+    for epoch in range(30):
+        loss, accuracy = train(epoch, model, trainLoader, optimizer, criterion)
+        train_loss_list.append(loss)
+        train_accuracy_list.append(accuracy)
+        valid_loss, valid_accuracy = valid(model, validLoader, criterion)
+        valid_loss_list.append(valid_loss)
+        valid_accuracy_list.append(valid_accuracy)
 
     prediction = test(model, testLoader)
+    # draw_loss(train_loss_list, valid_loss_list)
+    # draw_accuracy(train_accuracy_list, valid_accuracy_list)
     write_to_file(prediction)
